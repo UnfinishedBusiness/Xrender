e11f340689164e58b31537cbf36ae9bea3554ab6
Travis Gillin <travis.gillin@gmail.com>
2021-02-18 10:19:02 -0500

    Added gl.h in windows define

--- src/Xrender.cpp
+++ src/Xrender.cpp
@@ -25,6 +25,7 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
    //define something for Windows (32-bit and 64-bit, this part is common)
    #include <GL/freeglut.h>
+   #include <GL/gl.h>
    #ifdef _WIN64
       //define something for Windows (64-bit only)
    #else
